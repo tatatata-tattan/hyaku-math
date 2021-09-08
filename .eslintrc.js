c517c57392badebe1e20bf6439bcb95ad4fc7d69
tatatata_tattan <haltatsu09@gmail.com>
2021-09-08 23:05:51 +0900

    build(eslint, prettier): eslintとprettierの設定を追加

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:vue/vue3-recommended",
+    "@vue/typescript",
+    "plugin:prettier/recommended",
+    "prettier/vue",
+    "prettier/@typescript-eslint",
+  ],
+  rules: {},
+};
