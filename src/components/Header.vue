b54c4e449dc1d78c3401fc36822d3d5e3ff31197
tatatata_tattan <haltatsu09@gmail.com>
2021-09-09 19:00:17 +0900

    fix(header): ヘッダーの文言を修正

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="bg-indigo-300 py-2 px-4 text-white text-xl font-bold">
-    <router-link to="/">Let's Start 100 Math</router-link>
+    <router-link to="/">Let's 100 Math</router-link>
   </div>
 </template>
