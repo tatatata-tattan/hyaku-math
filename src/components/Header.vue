ec3fe779cac237ef9d97dbea54fe43cf6bd8af53
tatatata_tattan <haltatsu09@gmail.com>
2021-09-10 00:40:48 +0900

    fix(header): ヘッダーの背景色を変更

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-indigo-300 py-2 px-4 text-white text-xl font-bold">
+  <div class="bg-green-400 py-2 px-4 text-white text-xl font-bold">
     <router-link to="/">Let's 100 Math</router-link>
   </div>
 </template>
