45c93ce7a33f5af3a33f70109da79a897e1189a7
tatatata_tattan <haltatsu09@gmail.com>
2021-09-09 19:10:26 +0900

    fix(store): storeの設定が不足していたため追加

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import { createApp } from "vue";
 import router from "./router/index";
+import store from "./store";
 import App from "./App.vue";
 import "./index.css";
 
-createApp(App).use(router).mount("#app");
+const app = createApp(App);
+
+app.use(router);
+app.use(store);
+
+app.mount("#app");
